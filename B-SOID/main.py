3ad173cb013d25709f7ba3056d95f9fcd065fae1
Dhruv Laad <dhruvlaad@gmail.com>
2020-09-13 22:37:07 +0530

    batched umap

--- B-SOID/main.py
+++ B-SOID/main.py
@@ -4,7 +4,6 @@ from BSOID.bsoid import BSOID
 
 logging.basicConfig(level=logging.INFO)
 
-bsoid = BSOID('vinit_feats', '/scratch/scratch2/dhruvlaad/bsoid')
-bsoid.process_csvs(parallel=False)
-bsoid.features_from_points(temporal_dims=7)
-bsoid.cluster_feats(scale_feats=True)
+bsoid = BSOID('vinit_feats', '/home/dhruvlaad/data_custom')
+bsoid.umap_reduce(reduced_dim=10, sample_size=int(7e5), shuffle=True)
+bsoid.cluster_feats(reduced_feats=True)
