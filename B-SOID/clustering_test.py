de11542990c6f6b624431b62f7898d6940a4d6f6
Dhruv Laad <dhruvlaad@gmail.com>
2020-09-18 17:39:50 +0530

    faster exemplar calculation

--- B-SOID/clustering_test.py
+++ B-SOID/clustering_test.py
@@ -1,5 +1,5 @@
 import logging
-from BSOID.clustering import CURE
+from BSOID.clustering import bigCURE
 import joblib
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,7 +9,7 @@ with open('../../data/output/all_10D/all_umap_10D.sav', 'rb') as f:
 
 print(umap_embeddings.shape)
 
-cure = CURE(25, 100, 100, n_rep=1000, alpha=0.5)
+cure = bigCURE(25, 100, 100, 1000, 0.5)
 
 clusters = cure.preclustering(umap_embeddings)
 with open('preclusters.sav', 'wb') as f:
