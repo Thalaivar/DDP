ee6ec1b46616b0195469dce30d2bff56c20063b0
Thalaivar <dhruvlaad@gmail.com>
2020-10-07 10:10:32 +0530

    displacement

--- B-SOID/BSOID/bsoid.py
+++ B-SOID/BSOID/bsoid.py
@@ -16,7 +16,7 @@ from BSOID.data import *
 from BSOID.preprocessing import *
 from BSOID.prediction import *
 
-from BSOID.features.vinit_feats import *
+from BSOID.features.displacement_feats import *
 
 MLP_PARAMS = {
     'hidden_layer_sizes': (100, 10),  # 100 units, 10 layers
