d065506c77cdb1e6452d68b5d4b4aaae8e8b38ff
Thalaivar <dhruvlaad@gmail.com>
2020-10-11 15:29:33 +0530

    base tail, center spine and base neck in dis threshold

--- B-SOID/active_inactive_split.py
+++ B-SOID/active_inactive_split.py
@@ -12,7 +12,8 @@ feats = np.vstack(feats)
 
 # split data according to displacement threshold
 dis_threshold = 1.0
-displacements = feats[:,12].mean(axis=1)
+dis_idx = [8, 9, 12]
+displacements = feats[:,dis_idx].mean(axis=1)
 
 active_idx = np.where(displacements >= dis_threshold)[0]
 inactive_idx = np.where(displacements < dis_threshold)[0]
@@ -20,7 +21,7 @@ active_feats = feats[active_idx]
 inactive_feats = feats[inactive_idx]
 
 logging.info(f'divided data into active ({round(active_feats.shape[0]/feats.shape[0], 2)}%) and in-active ({round(inactive_feats.shape[0]/feats.shape[0], 2)}%) based on displacement threshold of {dis_threshold}')
-bsoid = BSOID('dis_active', '/home/dhruvlaad/data', 30, None, 3, None, True)
+bsoid = BSOID('dis_active_new', '/home/dhruvlaad/data', 30, None, 3, None, True)
 bsoid.save()
 
 import joblib
