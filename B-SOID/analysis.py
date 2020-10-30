b5265395d781383683409386549014c68edce6c1
Dhruv Laad <dhruvlaad@gmail.com>
2020-10-30 14:38:51 +0530

    add check for active split

--- B-SOID/analysis.py
+++ B-SOID/analysis.py
@@ -6,6 +6,9 @@ def generate_umap_embeddings(n: int, output_dir: str):
     base_dir = '/home/dhruvlaad/data'
     bsoid = BSOID.load_config(base_dir, run_id='dis')
 
+    if not hasattr(bsoid, 'active_split'):
+        bsoid.active_split = False
+        
     for i in tqdm(range(n)):
         results = bsoid.umap_reduce_all(reduced_dim=3, sample_size=int(4e5))
         with open(output_dir + f'/umap_{i}.pkl', 'wb') as f:
