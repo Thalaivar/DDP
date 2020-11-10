479e8125e4eeb08a944eecc6b4925a43e56e5f50
Thalaivar <dhruvlaad@gmail.com>
2020-11-10 18:01:10 +0530

    clip from center

--- B-SOID/BSOID/data.py
+++ B-SOID/BSOID/data.py
@@ -33,18 +33,13 @@ def extract_from_csv(filename, save_dir, trim=True):
         END_TRIM = FPS*2*60
         conf, pos = conf[END_TRIM:-END_TRIM,:], pos[END_TRIM:-END_TRIM,:]
 
-        eq_space = conf.shape[0] // 4
-        eq_spaced_idx = [i*eq_space for i in range(1,4)]
-
-        bsoid_data = []
-        CLIP_WINDOW = FPS*10*60 // 2
-        for idx in eq_spaced_idx:
-            clip_conf = conf[idx-CLIP_WINDOW:idx+CLIP_WINDOW,:]
-            clip_pos = pos[idx-CLIP_WINDOW:idx+CLIP_WINDOW,:]
-            bsoid_data.append(_bsoid_format(clip_conf, clip_pos))
+        clip_window = (FPS * RETAIN_WINDOW) // 2
+        mid_idx = conf.shape[0] // 2
+        clip_conf = conf[mid_idx-clip_window:mid_idx+clip_window,:]
+        clip_pos = pos[mid_idx-clip_window:mid_idx+clip_window,:]
+        bsoid_data = _bsoid_format(clip_conf, clip_pos)
+        bsoid_data.to_csv(save_dir + '/' + filename[:-3] + f'_{i}.csv', index=False)
         
-        for i, df in enumerate(bsoid_data):
-            df.to_csv(save_dir + '/' + filename[:-3] + f'_{i}.csv', index=False)
     else:
         bsoid_data = _bsoid_format(conf, pos)
         bsoid_data.to_csv(save_dir + '/' + filename[:-3] +'.csv', index=False)
