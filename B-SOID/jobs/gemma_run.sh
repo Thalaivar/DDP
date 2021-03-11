47628be6d5127d3270855a4ce365c8587cd8cc31
Dhruv Laad <laadd@sumner-log2.sumner.jax.org>
2021-03-11 07:57:46 -0500

    gemma trial

---
+++ B-SOID/jobs/gemma_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=BSOID-gemma
+#SBATCH --output=gemma.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=48:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=batch
+#SBATCH --mem=400000
+#SBATCH --mail-user=dhruv.laad@jax.org
+#SBATCH --mail-type=ALL
+
+cd /home/laadd/DDP/B-SOID/
+
+module load singularity
+
+nextflow run TheJacksonLaboratory/mousegwas --yaml gemma_config.yaml --shufyaml gemma_shuffle.yaml --input gemma_input.csv --outdir /home/laadd/gemma_output -profile slurm,singularity
