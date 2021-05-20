94277ff02b359389719bda5d447c2b01096f223d
Dhruv Laad <dhruvlaad@gmail.com>
2021-05-20 18:06:22 +0530

    launch jobs individually

--- B-SOID/jobs/2phase/pairwise_similarity.sh
+++ B-SOID/jobs/2phase/pairwise_similarity.sh
@@ -16,16 +16,14 @@ N_JOB=$N_TASKS
 BASE_DIR=/fastscratch/laadd/pwise_strain
 mkdir $BASE_DIR
 
-sim_measures=("dbcv_index_similarity" "density_separation_similarity" "roc_similiarity" "minimum_distance_similarity" "hausdorff_similarity")
+SIM_MEASURE="dbcv_index_similarity"
+SAVE_DIR=$BASE_DIR/$SIM_MEASURE
+mkdir $SAVE_DIR
+
+# sim_measures=("dbcv_index_similarity" "density_separation_similarity" "roc_similiarity" "minimum_distance_similarity" "hausdorff_similarity")
 
 source /home/laadd/.bashrc
 conda activate bsoid 
 
 cd /home/laadd/DDP/B-SOID/
-
-for measure in "${sim_measures[@]}"; do
-    mkdir $BASE_DIR/$JOBNAME-$measure
-    srun --exclusive --ntasks 1 python scripts.py --script calculate_pairwise_similarity --thresh 0.9 --save-dir $BASE_DIR/$JOBNAME-$measure --sim-measure $measure
-done
-
-wait
+python scripts.py --script calculate_pairwise_similarity --thresh 0.9 --save-dir $SAVE_DIR --sim-measure $SIM_MEASURE
