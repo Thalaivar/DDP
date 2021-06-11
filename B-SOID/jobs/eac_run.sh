c0f17d67895907b971cd47f127377dffd4b75c8d
Dhruv Laad <dhruvlaad@gmail.com>
2021-06-12 03:51:07 +0530

    eac runs

--- B-SOID/jobs/eac_run.sh
+++ B-SOID/jobs/eac_run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=laadd-stab
-#SBATCH --qos=batch
+#SBATCH --job-name=eac-run
+#SBATCH --output=eac-run-%J.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --time=48:00:00
 #SBATCH --partition=compute
+#SBATCH --qos=batch
 #SBATCH --mem=50000
 #SBATCH --mail-user=dhruv.laad@jax.org
-#SBATCH --output=laadd-stab-%j.out
 #SBATCH --mail-type=ALL
 
 source /home/laadd/.bashrc
 conda activate bsoid 
-cd /home/laadd/DDP/B-SOID/stability
+
+cd /home/laadd/DDP/B-SOID/BSOID
